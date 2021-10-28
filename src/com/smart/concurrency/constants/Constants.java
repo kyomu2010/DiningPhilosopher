3e3bed49972a8d04f6538a7ef873848ec94c20e7
mark <marksmart26@gmail.com>
2021-10-28 11:15:11 +0100

    updating constants, adding Philosopher methods

--- src/com/smart/concurrency/constants/Constants.java
+++ src/com/smart/concurrency/constants/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
 
 	public static final int NUMBER_OF_PHILOSOPHERS = 5;
 	public static final int NUMBER_OF_CHOPSTICKS = 5;
-	public static final int SIMULATION_ITERATIONS = 1000;
+	public static final int SIMULATION_RUNNING_TIME = 5*1000; // IN MILLISECONDS
 }
--- src/com/smart/concurrency/model/Philosopher.java
+++ src/com/smart/concurrency/model/Philosopher.java
@@ -1,5 +1,67 @@
 package com.smart.concurrency.model;
 
-public class Philosopher {
+import java.util.Random;
+
+import com.smart.concurrency.state.State;
+
+public class Philosopher implements Runnable {
+
+	private int id;
+	private Chopstick leftChopstick;
+	private Chopstick rightChopstick;
+	private Random random;
+	private int eatingCounter;
+	private volatile boolean isFull = false; // store isFull var in main RAM to ensure it is visible (consistent state)
+												// for all threads - if Philosopher is full stop thread
+
+	public Philosopher(int id, Chopstick leftChopstick, Chopstick rightChopstick) {
+		this.id = id;
+		this.leftChopstick = leftChopstick;
+		this.rightChopstick = rightChopstick;
+		this.random = new Random();
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			while (!isFull) {
+				think();
+				if (leftChopstick.canPickUp(this, State.LEFT)) {
+					if (rightChopstick.canPickUp(this, State.RIGHT)) {
+						eat();
+						rightChopstick.putDown(this, State.RIGHT);
+					}
+					leftChopstick.putDown(this, State.LEFT);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void think() throws InterruptedException {
+		System.out.println(this + " is thinking...");
+		Thread.sleep(random.nextInt(1000));
+	}
+
+	private void eat() throws InterruptedException {
+		System.out.println(this + " is eating...");
+		this.eatingCounter++;
+		Thread.sleep(random.nextInt(1000));
+	}
+
+	public void setFull(boolean isFull) {
+		this.isFull = isFull;
+	}
+
+	public int getEatingCounter() {
+		return eatingCounter;
+	}
+
+	@Override
+	public String toString() {
+		return "Philosopher " + id;
+	}
 
 }
